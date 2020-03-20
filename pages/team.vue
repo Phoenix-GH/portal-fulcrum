acf9325db88d84872c82b2ba511de0a7b28d201a
Phoenix214 <phoenix214@protonmail.com>
2020-03-21 00:05:01 +0530

    added dummy data for teams

--- pages/team.vue
+++ pages/team.vue
@@ -8,56 +8,23 @@
             th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
               | Name
             th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
-              | Title
+              | Description
             th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
-              | Email
+              | Status
             th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
-              | Role
+              | Created at
             th.px-6.py-3.border-b.border-gray-200.bg-gray-50
-        tbody.bg-white
+        tbody.bg-white(v-for="team in teams" v-bind:key="team.team_id")
           tr
             td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.font-medium.text-gray-900
-              | Bernard Lane
+              | {{team.team_name}}
             td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Director, Human Resources
+              | {{team.description}}
             td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | bernardlane@example.com
+              | {{team.team_status}}
             td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Owner
+              | {{Date(team.create_datetime)}}
             td.px-6.py-4.whitespace-no-wrap.text-right.border-b.border-gray-200.text-sm.leading-5.font-medium
-              a.text-indigo-600(href='#' class='hover:text-indigo-900 focus:outline-none focus:underline') Edit
-          tr
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.font-medium.text-gray-900
-              | Bernard Lane
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Director, Human Resources
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | bernardlane@example.com
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Owner
-            td.px-6.py-4.whitespace-no-wrap.text-right.border-b.border-gray-200.text-sm.leading-5.font-medium
-              a.text-indigo-600(href='#' class='hover:text-indigo-900 focus:outline-none focus:underline') Edit
-          tr
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.font-medium.text-gray-900
-              | Bernard Lane
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Director, Human Resources
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | bernardlane@example.com
-            td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
-              | Owner
-            td.px-6.py-4.whitespace-no-wrap.text-right.border-b.border-gray-200.text-sm.leading-5.font-medium
-              a.text-indigo-600(href='#' class='hover:text-indigo-900 focus:outline-none focus:underline') Edit
-          tr
-            td.px-6.py-4.whitespace-no-wrap.text-sm.leading-5.font-medium.text-gray-900
-              | Bernard Lane
-            td.px-6.py-4.whitespace-no-wrap.text-sm.leading-5.text-gray-500
-              | Director, Human Resources
-            td.px-6.py-4.whitespace-no-wrap.text-sm.leading-5.text-gray-500
-              | bernardlane@example.com
-            td.px-6.py-4.whitespace-no-wrap.text-sm.leading-5.text-gray-500
-              | Owner
-            td.px-6.py-4.whitespace-no-wrap.text-right.text-sm.leading-5.font-medium
               a.text-indigo-600(href='#' class='hover:text-indigo-900 focus:outline-none focus:underline') Edit
 
 </template>
@@ -67,7 +34,36 @@ export default {
   layout: 'default',
   name: 'TeamManagement',
   components: {},
-  data() {},
+  data() {
+    return {
+      teams: [
+        {
+          team_id: 1,
+          team_name: 'Team 1',
+          description: 'Dummy team 1',
+          team_status: 'active',
+          create_datetime: 20200306172513.426,
+          update_datetime: 20200306172513.426
+        },
+        {
+          team_id: 2,
+          team_name: 'Team 2',
+          description: 'Dummy team 2',
+          team_status: 'inactive',
+          create_datetime: 20200306172713.426,
+          update_datetime: 20200306172813.426
+        }
+      ]
+    }
+  },
+  async mounted() {
+    // const { data } = await this.$axios({
+    //   baseURL: 'http://api-v1.fulcrumsaas.net/api',
+    //   method: 'post',
+    //   url: '/auth/get'
+    // })
+    // console.log('auth_id----', data.response.auth.auth_id)
+  },
   methods: {}
 }
 </script>
