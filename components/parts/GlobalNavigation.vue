302fd5eb9ee7c1239bf0d0911c29ccd806922bf0
Phoenix214 <phoenix214@protonmail.com>
2020-03-20 21:33:03 +0530

    quick fix on navigation

--- components/parts/GlobalNavigation.vue
+++ components/parts/GlobalNavigation.vue
@@ -6,11 +6,11 @@
           .flex-shrink-0: img.h-8.w-8(src="/img/logos/workflow-mark-on-dark.svg" alt="")
           .hidden(class="md:block")
             .ml-6.flex.items-baseline
-              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-white.bg-gray-900(href="#" class="focus:outline-none focus:text-white focus:bg-gray-700") Dashboard
-              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Team
-              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Projects
-              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Calendar
-              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Reports
+              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-white.bg-gray-900(href="/" class="focus:outline-none focus:text-white focus:bg-gray-700") Dashboard
+              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="/team" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Team
+              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="/projects" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Projects
+              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="/calendar" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Calendar
+              a.ml-4.px-3.py-2.rounded-md.text-sm.font-medium.text-gray-300(href="/reports" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Reports
         .hidden(class="md:block")
           .ml-4.flex.items-center(class="md:ml-6")
             button.p-1.border-2.border-transparent.text-gray-400.rounded-full(class="hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"): svg.h-6.w-6(stroke="currentColor" fill="none" viewBox="0 0 24 24"): path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9")
@@ -43,7 +43,7 @@
           a.block.px-3.py-2.rounded-md.text-base.font-medium.text-gray-400(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Your Profile
           a.mt-1.block.px-3.py-2.rounded-md.text-base.font-medium.text-gray-400(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Settings
           a.mt-1.block.px-3.py-2.rounded-md.text-base.font-medium.text-gray-400(href="#" class="hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700") Sign out
-  
+
 </template>
 
 <script>
