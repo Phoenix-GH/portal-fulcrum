7e7330c571c43650c4218b7efe0d64c99bba7bbe
Phoenix214 <phoenix214@protonmail.com>
2020-03-24 11:42:41 -0700

    added team table header

--- pages/team.vue
+++ pages/team.vue
@@ -2,6 +2,10 @@
 .flex.flex-col
   .-my-2.py-2.overflow-x-auto(class='sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8')
     .align-middle.inline-block.min-w-full.shadow.overflow-hidden.border-b.border-gray-200(class='sm:rounded-lg')
+      .bg-white.px-4.py-5.border-b.border-gray-200(class="sm:px-6")
+        .-ml-4.-mt-2.flex.items-center.justify-between.flex-wrap(class="sm:flex-no-wrap")
+          .ml-4.mt-2: h3.text-lg.leading-6.font-medium.text-gray-900 Teams
+          .ml-4.mt-2.flex-shrink-0: span.inline-flex.rounded-md.shadow-sm: button.relative.inline-flex.items-center.px-4.py-2.border.border-transparent.text-sm.leading-5.font-medium.rounded-md.text-white.bg-indigo-600(type="button" class="hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700") Create new team
       table.min-w-full
         thead
           tr
