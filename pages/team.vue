6858e73b0c8cecd9b20cd380f4f47f9fbadeb5e7
Phoenix214 <phoenix214@protonmail.com>
2020-03-31 12:41:41 -0700

    added invitation list

--- pages/team.vue
+++ pages/team.vue
@@ -9,12 +9,12 @@
               h3.text-lg.leading-6.font-medium.text-gray-900 Team
               .mt-6(class="sm:mt-5")
                 div(class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5")
-                  label.block.text-sm.font-medium.leading-5.text-gray-700(for="username" class="sm:mt-px sm:pt-2") Instances list
+                  label.block.text-sm.font-medium.leading-5.text-gray-700(for="username" class="sm:mt-px sm:pt-2") Instances
                   .mt-1(class="sm:mt-0 sm:col-span-2")
 
 
               .mt-6(class="sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5")
-                label.block.text-sm.font-medium.leading-5.text-gray-700(for="about" class="sm:mt-px sm:pt-2") Members list
+                label.block.text-sm.font-medium.leading-5.text-gray-700(for="about" class="sm:mt-px sm:pt-2") Members
                 .mt-1(class="sm:mt-0 sm:col-span-2")
                   table.min-w-full
                     thead
@@ -43,8 +43,34 @@
                           span &nbsp;|&nbsp;
                           a.text-red-600.leading-4(v-on:click='showDeleteUserModal(user.user_id)' class='hover:text-indigo-900 focus:outline-none focus:underline') Delete
               .mt-6(class="sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5")
-                label.block.text-sm.font-medium.leading-5.text-gray-700(for="about" class="sm:mt-px sm:pt-2") Invitations list
+                label.block.text-sm.font-medium.leading-5.text-gray-700(for="about" class="sm:mt-px sm:pt-2") Invitations
                 .mt-1(class="sm:mt-0 sm:col-span-2")
+                  table.min-w-full
+                    thead
+                      tr
+                        th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
+                          | Message
+                        th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
+                          | Status
+                        th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
+                          | Team Role
+                        th.px-6.py-3.border-b.border-gray-200.bg-gray-50.text-left.text-xs.leading-4.font-medium.text-gray-500.uppercase.tracking-wider
+                          | Email
+                        th.px-6.py-3.border-b.border-gray-200.bg-gray-50
+                    tbody.bg-white(v-for="invitation in invitations" v-bind:key="invitation.invitation_code")
+                      tr
+                        td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
+                          | {{invitation.message}}
+                        td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
+                          | {{invitation.invitation_status}}
+                        td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
+                          | {{invitation.team_role}}
+                        td.px-6.py-4.whitespace-no-wrap.border-b.border-gray-200.text-sm.leading-5.text-gray-500
+                          | {{invitation.email}}
+                        td.px-6.py-4.whitespace-no-wrap.text-right.border-b.border-gray-200.text-sm.leading-5.font-medium
+                          a.text-indigo-600(href='#' class='hover:text-indigo-900 focus:outline-none focus:underline') Edit
+                          span &nbsp;|&nbsp;
+                          a.text-red-600.leading-4(v-on:click='showDeleteUserModal(user.user_id)' class='hover:text-indigo-900 focus:outline-none focus:underline') Delete
 
 </template>
 
@@ -56,7 +82,8 @@ export default {
   data() {
     return {
       teamId: null,
-      users: []
+      users: [],
+      invitations: []
     }
   },
   mounted() {
@@ -74,7 +101,8 @@ export default {
         }
       })
       this.users = data.response.users
-      console.log('users---', this.users)
+      this.invitations = data.response.invitations
+      console.log('invitations---', this.invitations)
     },
     showDeleteUserModal() {}
   }
