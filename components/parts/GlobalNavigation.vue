8e0a8dd08eeff3087e433117c667006e85e2271a
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-04-10 03:54:20 -0400

    notification bug fix

--- components/parts/GlobalNavigation.vue
+++ components/parts/GlobalNavigation.vue
@@ -35,12 +35,11 @@
 
             .ml-3.relative
               button.p-1.border-2.border-transparent.text-gray-400.rounded-full(class="hover:text-white focus:outline-none focus:text-white focus:bg-gray-700" @click="onOpen('notifications')" title="Manage Notifications")
-                template(v-if="hasNotifications")
-                  div.relative
-                    svg.h-6.w-6(:stroke="[ hasNotifications ? 'currentColor' : null ]" :fill="[ hasNotifications ? 'none' : 'currentColor' ]" viewBox="0 0 24 24")
-                      path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9")
-                    span.absolute.top-0.right-0.block.rounded-full.text-gray-800.shadow-solid.bg-red-600(class="h-1.5 w-1.5" v-if="hasNotifications")
-                    
+                div.relative
+                  svg.h-6.w-6(:stroke="[ hasNotifications ? 'currentColor' : null ]" :fill="[ hasNotifications ? 'none' : 'currentColor' ]" viewBox="0 0 24 24")
+                    path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9")
+                  span.absolute.top-0.right-0.block.rounded-full.text-gray-800.shadow-solid.bg-red-600(class="h-1.5 w-1.5" v-if="hasNotifications")
+                  
               transition( name="fade")
                 .origin-top-right.z-20.absolute.right-0.mt-2.w-96.rounded-md.shadow-lg(v-show="isOpen('notifications')")
                   .py-1.rounded-md.bg-white.shadow-xs
